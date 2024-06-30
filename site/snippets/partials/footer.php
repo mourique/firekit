debc9954a41a97600067705bad0ee04686a4a73a
Felix F <mail@felixf.de>
2024-06-30 15:00:07 +0200

    simpler footer

--- site/snippets/partials/footer.php
+++ site/snippets/partials/footer.php
@@ -4,11 +4,7 @@
       <div class="col-xs-12">
         <div class="blocks">
           <div class="block block-test">
-            <p>Nostrud eu et exercitation. Et exercitation anim, cillum nostrud. Cillum nostrud exercitation, est
-              eiusmod
-              aute proident tempor. Est eiusmod aute proident tempor amet fugiat proident. Aute proident tempor amet
-              fugiat proident proident enim. Tempor amet, fugiat proident proident enim id. Proident proident enim id
-              irure eu. Enim id irure, eu tempor aute magna ipsum. Eu tempor aute magna. Aute magna ipsum enim ea.</p>
+            <pre>This is the Footer Area </pre>
           </div>
         </div>
       </div>
